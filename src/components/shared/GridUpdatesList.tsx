98e4783c696f5ad7f0df31d3c5c5058258b150d8
prashantjadon <bughunterprashant@gmail.com>
2024-05-06 09:51:39 +0530

    33

--- src/components/shared/GridUpdatesList.tsx
+++ src/components/shared/GridUpdatesList.tsx
@@ -15,32 +15,28 @@ const GridUpdatesList = ({
   const { user } = useUserContext();
 
   return (
-    <div>
-      <ul className="grid-container">
-        {updates.map((update) => (
-          <li key={update.$id} className="relative min-w-80 h-auto mb-4">
-            <Link to={`/updates/${update.$id}`} className="block">
-              {update.imageUrlRequest && (
-                <img
-                  src={update.imageUrlRequest}
-                  alt="post"
-                  className="w-full h-auto rounded-lg"
-                />
-              )}
-              <div className="bg-black bg-opacity-50 text-white p-4 rounded-lg">
-                <p className="text-lg font-semibold mb-2">
-                  {update.captionRequest}
-                </p>
-              </div>
-            </Link>
-            {showStats && (
-              <div className="mt-2">
-                <UpdateStats update={update} userId={user.id} />
-              </div>
+    <div className="grid-container">
+      {updates.map((update) => (
+        <div key={update.$id} className="post-card">
+          <Link to={`/updates/${update.$id}`} className="block">
+            {update.imageUrlRequest && (
+              <img
+                src={update.imageUrlRequest}
+                alt="post"
+                className="post-card_img"
+              />
             )}
-          </li>
-        ))}
-      </ul>
+            <div className="post-content p-4">
+              <p className="post-caption">{update.captionRequest}</p>
+            </div>
+          </Link>
+          {showStats && (
+            <div className="post-stats p-4">
+              <UpdateStats update={update} userId={user.id} />
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
