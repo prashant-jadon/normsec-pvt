881f8943e8ca88a1dcc8fedd6ffeb8bae4b1b9f2
prashantjadon <bughunterprashant@gmail.com>
2024-05-08 02:44:18 +0530

    36

--- src/_auth/forms/FormPasswordRest.tsx
+++ src/_auth/forms/FormPasswordRest.tsx
@@ -5,6 +5,7 @@ import { useToast } from "@/components/ui/use-toast";
 import { UpdateRecoverValidation } from "@/lib/Validation";
 import { account } from "@/lib/appwrite/config";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form"
 import { useNavigate } from "react-router-dom";
 import { z } from "zod";
@@ -12,91 +13,92 @@ import { z } from "zod";
 const FormPasswordRest = () => {
 
     const { toast } = useToast();
- 
-      const navigate = useNavigate();
-  
-
-  
-      const form = useForm<z.infer<typeof UpdateRecoverValidation>>({
-          resolver: zodResolver(UpdateRecoverValidation),
-          defaultValues: {
+    const navigate = useNavigate();
+    
+    const [password, setPassword] = useState({
+        newPassword: "",
+        repeatPassword: "",
+    });
+    
+    const form = useForm<z.infer<typeof UpdateRecoverValidation>>({
+        resolver: zodResolver(UpdateRecoverValidation),
+        defaultValues: {
             password: "",
             repassword: "",
-          },
-        })
+        },
+    });
 
-  
-  
-        async function onSubmit(values: z.infer<typeof UpdateRecoverValidation>) {
-            const urlParams = new URLSearchParams(window.location.search);
-            const userId = urlParams.get('userId');
-            const secret = urlParams.get('secret');
-            if(userId==null){
-                return;
-            }
-            if(secret==null){
-                return;
-            }
-          
-           const session = await account.updateRecovery(userId,secret,values.password);
-           console.log(session);
-
-         if(!session){
-          return toast({title: "Sign in failed. Please try again."})
-         }
-          
-         if(session){
-            form.reset();
-            navigate('/sigin');
-           }else{
-            toast({title:'Reset password fail Failed.Please try again'})
-           }
+    async function onSubmit(values: z.infer<typeof UpdateRecoverValidation>) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const userId = urlParams.get('userId');
+        const secret = urlParams.get('secret');
+        
+        if (userId === null || secret === null) {
+            return;
+        }
+        
+        if (values.password !== values.repassword) {
+            return toast({ title: "Passwords do not match" });
+        }
+        
+        const session = await account.updateRecovery(userId, secret, values.password);
+        
+        if (!session) {
+            return toast({ title: "Sign in failed. Please try again." });
+        }
+        
+        form.reset();
+        navigate('/signin');
     }
-  return (
-    <Form {...form}>
-    <div className='sm:w-420 flex-center flex-col'>
-        <img src='/assets/images/logo.svg' alt='logo'/>
-        <h2 className='h3-bold md:h2-bold pt-1 sm:pt-12'>Create a New Account</h2>
-        <p className='text-light-3 small-medium md:base-regular mt-2'>To use RentAround enter your details</p>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
-                 
-        <FormField 
-              control={form.control} 
-              name="password"
-              render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                          <Input type='password' className='shad-input' {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                )}
-              />
 
-
-              <FormField 
-              control={form.control} 
-              name="repassword"
-              render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                          <Input type='password' className='shad-input' {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                )}
-              />
-
-                       
-         
-          <Button type="submit" className='shad-button_primary'>"Sign Up" </Button>
-
-        </form>
-</div>
-</Form>
-  )
+    return (
+        <Form {...form}>
+            <div className='sm:w-420 flex-center flex-col'>
+                <img src='/assets/images/logo.svg' alt='logo'/>
+                <h2 className='h3-bold md:h2-bold pt-1 sm:pt-12'>Create a New Account</h2>
+                <p className='text-light-3 small-medium md:base-regular mt-2'>To use RentAround enter your details</p>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+                    <FormField 
+                        control={form.control} 
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input 
+                                        type='password' 
+                                        className='shad-input' 
+                                        {...field} 
+                                        onChange={(e) => setPassword({ ...password, newPassword: e.target.value })} 
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField 
+                        control={form.control} 
+                        name="repassword"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Repeat Password</FormLabel>
+                                <FormControl>
+                                    <Input 
+                                        type='password' 
+                                        className='shad-input' 
+                                        {...field} 
+                                        onChange={(e) => setPassword({ ...password, repeatPassword: e.target.value })} 
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className='shad-button_primary'>Sign Up</Button>
+                </form>
+            </div>
+        </Form>
+    )
 }
 
-export default FormPasswordRest
+export default FormPasswordRest;
