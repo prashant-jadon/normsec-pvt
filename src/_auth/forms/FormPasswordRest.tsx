2bf8a70d0a50d7ff2dfbee567f3529bbb65f6b33
prashantjadon <bughunterprashant@gmail.com>
2024-05-09 09:10:34 +0530

    36

--- src/_auth/forms/FormPasswordRest.tsx
+++ src/_auth/forms/FormPasswordRest.tsx
@@ -1,25 +1,49 @@
 import { useState } from 'react';
+import { account } from '@/lib/appwrite/config';
 import { useUserContext } from '@/context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { toast } from '@/components/ui/use-toast';
 
 const FormPasswordRest = () => {
-    const { isAuthenticated } = useUserContext(); // Get isAuthenticated state from context
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const {checkAuthUser} = useUserContext();
+    const navigate = useNavigate();
 
+
+
+   
     const submit = async () => {
-     
-        // Your password reset logic here
+       
+
+        let urlSearchParams = new URLSearchParams(window.location.search);
+        let secret = urlSearchParams.get("secret");
+        let userId = urlSearchParams.get("userId");
+
+        if (userId && secret) {
+            try {
+                await account.updateRecovery(userId, secret, password);
+            } catch (error) {
+                alert(error);
+            }
+        } else {
+            alert('User ID or secret not found in the URL.');
+        }
+
+        const isLoggedIn = await checkAuthUser();
+        if(isLoggedIn){
+         navigate('/');
+        }else{
+         toast({title:'CONSOLE LOG REDIRECG'})
+        }
     };
 
-    if (isAuthenticated) {
-        // Redirect to dashboard or any other desired page if user is already authenticated
-        window.location.href = "/#/dashboard";
-        return null; // Prevent rendering the password reset form
-    }
+    // Redirect if user is logged in
 
     return (
         <div>
             <h1>Reset your password</h1>
+
             <form onSubmit={submit}>
                 <label htmlFor="password"><b>New Password</b></label>
                 <input
@@ -28,8 +52,8 @@ const FormPasswordRest = () => {
                     name="password"
                     value={password}
                     onChange={(event) => setPassword(event.target.value)}
-                    required
-                />
+                    required />
+
                 <label htmlFor="confirmPassword"><b>Confirm Password</b></label>
                 <input
                     type="password"
@@ -37,8 +61,8 @@ const FormPasswordRest = () => {
                     name="confirmPassword"
                     value={confirmPassword}
                     onChange={(event) => setConfirmPassword(event.target.value)}
-                    required
-                />
+                    required />
+
                 <button className="button" type="submit">Reset Password</button>
             </form>
         </div>
